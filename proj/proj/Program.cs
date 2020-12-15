f65b277d9d5cb676d3b4fa39f2c9cc3d4db5c0f5
Zach Doster <doster.zach@gmail.com>
2020-12-14 22:19:11 -0600

    Pull in test code and add test file

--- proj/proj/Program.cs
+++ proj/proj/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+
 
 namespace proj
 {
@@ -6,7 +9,21 @@ namespace proj
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-		}
+            // TODO: allow for args?
+            string path = "../../../test/python_test_code.py";
+
+
+            // Open the stream and read it back.
+            using (FileStream fs = File.Open(path, FileMode.Open))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    Console.WriteLine(temp.GetString(b));
+                }
+            }
+        }
 	}
 }
