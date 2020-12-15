1b7137f38f69fe1aba4227bef403068a18959d68
Zach Doster <doster.zach@gmail.com>
2020-12-14 22:27:44 -0600

    Init test file and basic open

--- proj/proj/Program.cs
+++ proj/proj/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+
 
 namespace proj
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: allow for args?
+            string path = "../../../test/python_test_code.py";
+
+
+            // Open the stream and read it back.
+            using (FileStream fs = File.Open(path, FileMode.Open))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    Console.WriteLine(temp.GetString(b));
+                }
+            }
+        }
+    }
 }
