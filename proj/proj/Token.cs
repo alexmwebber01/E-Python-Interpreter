59f57048bc16c6cb6de416ac192ba5d5e38bd1c1
alexmwebber <alexmwebber@gmail.com>
2020-12-15 21:17:29 -0600

    Added Lexer and changed Token

--- proj/proj/Token.cs
+++ proj/proj/Token.cs
@@ -1,14 +1,15 @@
 
 namespace proj
 {
-    public class Token<T>
+    public class Token
     {
         // token type: INTEGER, STRING, EOF, ...
         public string type { get; set; }
-        // token value, generic type
-        public T value { get; set; }
+        // token value, e.g. "24" for an int "alsjdf" for a string
+        // will have to typecast this to get the actual value according to the type
+        public string value { get; set; }
 
-        public Token(string _type, T _value)
+        public Token(string _type, string _value)
         {
             type = _type;
             value = _value;
